4f9387ce859b28bd9b9d2220c3f2023baafe269f
Karen Soriano <karensoriano@karens-mbp.attlocal.net>
2018-07-02 21:57:30 -0500

    Changes made 7/02/2018

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-
+// instantiate
+// player
 const player = new Player();
+
+// 3 enemies
 const allEnemies = [...Array(3)].map((_,i)=> new Enemy(0,i+1));
 
+// listening for certain key strokes
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
--- js/classes.js
+++ js/classes.js
@@ -1,11 +1,14 @@
+// entity consists of both things both player & enemies have in common
 class Entity {
 	constructor(){
 		this.sprite = 'images/';
-		this.x = 4;
+		// starts sprite off at a certain place
+		this.x = 2;
 		this.y = 5;
 	}
 
 	update(dt) {
+		// setting where boundaries are
 		this.isOutofBoundsX = this.x > 5;
 		this.isOutofBoundsY = this.y < 1;
 	}
@@ -14,6 +17,7 @@ class Entity {
 		ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
 	}
 
+	// checking for collisions
 	checkCollisions(playerOrEnemy) {
 		if (this.y === playerOrEnemy.y) {
 			if (this.x >= playerOrEnemy.x - 0.5 && this.x <= playerOrEnemy.x + 0.5) {
@@ -26,6 +30,7 @@ class Entity {
 	}
 }
 
+// specific to player sprite, uses some of entity 
 class Player extends Entity {
 	constructor() {
 		super();
@@ -36,6 +41,7 @@ class Player extends Entity {
 
 	update(dt) {
 		super.update();
+		// if user wins, display alert 
 		if (this.isOutofBoundsY && !this.moving && !this.win){
 			alert("You win!");
 			this.win = true;
@@ -49,6 +55,7 @@ class Player extends Entity {
 		this.moving = false;
 	}
 
+	// user inputs controlling sprite: left, right, up, down
 	handleInput(input) {
 		switch (input) {
 			case 'left':
@@ -70,16 +77,19 @@ class Player extends Entity {
 	}
 }
 
+// enemy-specific, uses some of entity
 class Enemy extends Entity {
 	constructor(x, y) {
 		super();
 		this.sprite += 'enemy-bug.png';
 		this.x = x;
 		this.y = y;
+		// set random speed
 		this.speed = Math.random() * (6);
 
 	}
 
+// if they go out of bounds, start the bugs off screen again
 	update(dt){
 		super.update();
 		if (this.isOutofBoundsX){
